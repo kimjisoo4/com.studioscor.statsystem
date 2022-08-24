46a4fd528d516536d3f75078a5712859dafdb9a5
kimjisoo4 <58765606+kimjisoo4@users.noreply.github.com>
2022-08-25 07:39:29 +0900

    그것을 간직하다.
     [ Changed ]
     StatBlockUI
     - 이제 스텟을 저장해놓고, 해당 스텟에서 값을 가져온다.

--- Scripts/Debug/StatBlockUI.cs
+++ Scripts/Debug/StatBlockUI.cs
@@ -9,10 +9,27 @@ namespace KimScor.StatSystem
         [SerializeField] private Text Name;
         [SerializeField] private Text Count;
 
+        private Stat _Stat;
+
         public void SetText(KeyValuePair<StatTag, Stat> stat)
         {
-            Name.text = stat.Key.StatName;
-            Count.text = Mathf.RoundToInt(stat.Value.Value).ToString();
+            if (_Stat is not null)
+            {
+                _Stat.OnChangedValue -= Stat_OnChangedValue;
+            }
+
+            _Stat = stat.Value;
+
+            Name.text = _Stat.StatName;
+            Count.text = Mathf.RoundToInt(_Stat.Value).ToString();
+
+            _Stat.OnChangedValue += Stat_OnChangedValue;
+        }
+
+        private void Stat_OnChangedValue(Stat stat, float currentValue, float prevValue)
+        {
+            Name.text = _Stat.StatName;
+            Count.text = Mathf.RoundToInt(_Stat.Value).ToString();
         }
     }
 }
