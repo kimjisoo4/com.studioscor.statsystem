e0c8f57af3555d52691b8922adf3f0feb098a1f1
KimScor <58765606+kimjisoo4@users.noreply.github.com>
2023-08-13 10:35:58 +0900

    Changed
    [ Changed ]
     변수 이름 및 메뉴 경로 수정

--- Core/StatTag.cs
+++ Core/StatTag.cs
@@ -2,21 +2,21 @@
 
 namespace StudioScor.StatSystem
 {
-    [CreateAssetMenu(fileName ="Stat_", menuName = "StudioScor/Attribute System/new Stat")]
+    [CreateAssetMenu(fileName ="Stat_", menuName = "StudioScor/Stat System/new Stat")]
     public class StatTag : ScriptableObject
     {
-        [Header("[Name]")]
-        [SerializeField] private string _Name;
-        [Header("[Text]")]
-        [SerializeField] private string _Description;
+        [Header(" [ Name ] ")]
+        [SerializeField] private string statName;
+        [Header(" [ Text ] ")]
+        [SerializeField] private string description;
 
-        public string Name => _Name;
-        public string Description => _Description;
+        public string Name => statName;
+        public string Description => description;
 
-        public void SetStatTag(string newName, string description = null)
+        public void SetStatTag(string newName, string newDescription = null)
         {
-            _Name = newName;
-            _Description = description;
+            statName = newName;
+            description = newDescription;
         }
     }
 }
