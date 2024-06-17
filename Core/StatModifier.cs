30dd712850a3bb79500f103eec9eace16a96348d
KimScor <58765606+kimjisoo4@users.noreply.github.com>
2024-06-17 12:43:16 +0900

    Changed Variable Name
    [ Changed ]
     변수 이름 조정.
     Setup 함수 추가

--- Core/StatModifier.cs
+++ Core/StatModifier.cs
@@ -7,30 +7,37 @@ namespace StudioScor.StatSystem
 	public class StatModifier
 	{
         [Header(" [ Stat Modifier ] ")]
-		[SerializeField] private float _Value;
-		[SerializeField] private EStatModifierType _Type;
-		[SerializeField] private int _Order;
-		[SerializeField] private object _Source;
+		[SerializeField] private float _value = 0f;
+		[SerializeField] private EStatModifierType _type = EStatModifierType.Absolute;
+		[SerializeField] private int _order = 0;
+		[SerializeField] private object _source = null;
 
-		public float Value => _Value;
-		public EStatModifierType Type => _Type;
-		public int Order => (int)Type + _Order;
-		public object Source => _Source;
+		public float Value => _value;
+		public EStatModifierType Type => _type;
+		public int Order => (int)Type + _order;
+		public object Source => _source;
 
+		public StatModifier()
+		{
+
+		}
 		public StatModifier(StatModifier statModifier)
         {
-			_Value = statModifier.Value;
-			_Type = statModifier.Type;
-			_Order = statModifier.Order;
-			_Source = statModifier.Source;
+            Setup(statModifier._value, statModifier._type, statModifier._order, statModifier._source);
 		}
 
 		public StatModifier(float value, EStatModifierType type, int order = 0, object source = null)
 		{
-			_Value = value;
-			_Type = type;
-			_Order = order;
-			_Source = source;
-		}
+			Setup(value, type, order, source);
+
+        }
+
+		public void Setup(float value, EStatModifierType type, int order = 0, object source = null)
+        {
+            _value = value;
+            _type = type;
+            _order = order;
+            _source = source;
+        }
 	}
 }
